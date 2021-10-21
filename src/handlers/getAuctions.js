ddaa2102de32ad2157aacac8d5f8645821ae5bd2
sordoneza <sordonez88@gmail.com>
2021-10-21 11:12:23 -0600

    feat: added schema validation and made use of query instead of scan

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,20 +1,30 @@
-import AWS from 'aws-sdk';
-import createError from 'http-errors';
-import commonMiddleware from '../lib/commonMiddleware';
-
+import AWS from "aws-sdk";
+import createError from "http-errors";
+import validator from "@middy/validator";
+import commonMiddleware from "../lib/commonMiddleware";
+import getAuctionsSchema from "../lib/schemas/getAuctionsSchema";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
   let auctions;
 
-  try {
-    const result = await dynamodb
-      .scan({
-        TableName: process.env.AUCTIONS_TABLE_NAME
-      })
-      .promise();
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: "statusAndEndDate",
+    KeyConditionExpression: "#status = :status",
+    ExpressionAttributeValues: {
+      ":status": status,
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
 
-    auctions =result.Items;
+  try {
+    const result = await dynamodb.query(params).promise();
+
+    auctions = result.Items;
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
@@ -26,6 +36,12 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
-
-
+export const handler = commonMiddleware(getAuctions).use(
+  validator({
+    inputSchema: getAuctionsSchema,
+    ajvOptions: {
+      useDefaults: true,
+      strict: false,
+    },
+  })
+);
