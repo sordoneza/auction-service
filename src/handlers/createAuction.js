7a335248c90ca4120b38eb1f661c3d35b9bbd720
sordoneza <sordonez88@gmail.com>
2021-10-21 11:11:28 -0600

    feat: added endingAt property and schema validation

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -2,18 +2,22 @@ import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
 import commonMiddleware from '../lib/commonMiddleware';
-
+import createAuctionSchema from "../lib/schemas/createAuctionSchema";
+import validator from '@middy/validator';
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
   const { title } = event.body;
   const now = new Date();
+  const endDate = new Date();
+  endDate.setHours(now.getHours() + 1);
 
   const auction = {
     id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
+    endingAt: endDate.toISOString(),
     highestBid: {
       amount: 0
     }
@@ -38,6 +42,13 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction).use(
+  validator({
+    inputSchema: createAuctionSchema,
+    ajvOptions: {
+      strict: false,
+    },
+  })
+);
 
 
