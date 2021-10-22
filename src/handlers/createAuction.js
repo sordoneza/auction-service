c82b108b6aa4b81d43177873f524b6f4984291a1
sordoneza <sordonez88@gmail.com>
2021-10-22 10:11:00 -0600

    feat: added seller property from the authorizer context

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -8,6 +8,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
   const { title } = event.body;
+  const { email } = event.requestContext.authorizer;
   const now = new Date();
   const endDate = new Date();
   endDate.setHours(now.getHours() + 1);
@@ -20,7 +21,8 @@ async function createAuction(event, context) {
     endingAt: endDate.toISOString(),
     highestBid: {
       amount: 0
-    }
+    },
+    seller: email
   };
 
   try {
